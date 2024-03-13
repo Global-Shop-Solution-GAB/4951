7d005109836a1a2b5fc670b0e7c709728b619b4f
Service Web <sw@gssmail.com>
2024-03-13 09:07:58 -0500

    Commit changes in C:\Websites\Git\emarlow\4951\GCG_4951_QA_Data.sql

---
+++ GCG_4951_QA_Data.sql
@@ -0,0 +1,15 @@
+/* ==========================================
+ * TABLE: GCG_4951_QA_DATA
+ * ========================================== */
+CREATE TABLE "GCG_4951_QA_DATA"(
+ "QUALITY_NO" CHAR(7),
+ "QA_CODE" CHAR(100),
+ "Others" LONGVARCHAR,
+ "AMOUNT" CHAR(10),
+ "RECORD_NO" CHAR(4),
+ "DISCREP_CODE" CHAR(100),
+ "DISPOSAL_ACTION" CHAR(100),
+ "Serial" LONGVARCHAR,
+ "RefDes" LONGVARCHAR);
+
+
