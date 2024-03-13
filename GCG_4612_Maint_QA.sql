f3f482aaf20206ffc3bce766c9e166d53963c72d
Service Web <sw@gssmail.com>
2024-03-13 09:07:58 -0500

    Commit changes in C:\Websites\Git\emarlow\4951\GCG_4612_Maint_QA.sql

---
+++ GCG_4612_Maint_QA.sql
@@ -0,0 +1,8 @@
+/* ==========================================
+ * TABLE: GCG_4612_Maint_QA
+ * ========================================== */
+CREATE TABLE "GCG_4612_Maint_QA"(
+ "QA" CHAR(6),
+ "DESCRIPTION" LONGVARCHAR);
+
+
